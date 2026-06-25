52cb14c03a30ba907d3fbc535671c54d04115681
Udit Patel <patelridit676@gmail.com>
2026-06-25 08:16:19 +0530

    Create insertion_sort.cpp

---
+++ insertion_sort.cpp
@@ -0,0 +1,33 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+void insertion_sort(int arr[], int n){
+
+    for(int i=0; i<n-1; i++){
+        int minIndex = i;
+        for(int j=i+1; j<n; j++ ){
+            if(arr[minIndex]>arr[j]){
+                minIndex = j;
+            }
+        }
+        swap(arr[minIndex],arr[i]);
+    }
+
+};
+
+int main(){
+     int n;
+    cin>>n;
+    int arr[n];
+    for(int i=0; i<n; i++){
+        cin>> arr[i];
+    }
+
+    insertion_sort(arr,n);
+
+    for(int i=0; i<n; i++){
+        cout << arr[i] <<" ";
+    }
+    return 0;
+
+}
