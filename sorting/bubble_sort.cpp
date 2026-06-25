d4457990a0e4bbe1d56de6ced7dd6af708b5055f
Udit Patel <patelridit676@gmail.com>
2026-06-25 07:59:24 +0530

    Update bubble_sort.cpp

--- sorting/bubble_sort.cpp
+++ sorting/bubble_sort.cpp
@@ -3,12 +3,18 @@ using namespace std;
 
 void bubble_sort(int arr[], int n){
     for(int i=n-1; i>=0; i--){
+        int didswap = 0;
         for(int j=0; j<=i-1; j++){
             if(arr[j]>arr[j+1]){
                 swap(arr[j],arr[j+1]);
+                didswap = 1;
             }
         }
-    }
+        if(didswap == 0 ){
+            break;
+        }
+        cout <<"run";
+    }  
 
 }
 
